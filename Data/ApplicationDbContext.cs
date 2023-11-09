4683388cfe3ec655bcccb67b5e00af6c1af8e43f
khattab88 <m_khatab_88@hotmail.com>
2023-11-09 14:15:12 +0200

    add migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DockerComposeSample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerComposeSample.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
+using DockerComposeSample.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://*:8080");
 
 // Add services to the container.
+
+var server = builder.Configuration["Database:Server"] ?? "";
+var port = builder.Configuration["Database:Port"] ?? "";
+var user = builder.Configuration["Database:User"] ?? "";
+var password = builder.Configuration["Database:Password"] ?? "";
+var database = builder.Configuration["Database:Name"] ?? "";
+
+var connectionString = $"Server={server},{port}; UserId={user}; Password={password}; InitialCatalog={database};";
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+{
+    options.UseSqlServer(connectionString);
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
